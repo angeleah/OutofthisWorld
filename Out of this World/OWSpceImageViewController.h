fbdbd8575dbea54d62eb2db147abde7a990ef35a
Angeleah Daidone <angeleah@angeleah.com>
2014-03-26 13:21:55 -0500

    Create the SpaceImageViewController.

---
+++ Out of this World/OWSpceImageViewController.h
@@ -0,0 +1,14 @@
+//
+//  OWSpceImageViewController.h
+//  Out of this World
+//
+//  Created by Angeleah Daidone on 3/26/14.
+//  Copyright (c) 2014 Angeleah Daidone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWSpceImageViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
